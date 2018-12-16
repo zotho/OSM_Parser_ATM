8f49e077864c9b2a4546f3a59ce96c37ec074a35
Zotho <svjatoslavalekseef2@gmail.com>
2018-12-16 19:12:42 +0000

    Added setup.sh

---
+++ setups.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo pacman -Sy yaourt &&
+yaourt -S --noconfirm sublime-text-dev python-numpy tk python-pandas jupyter-notebook python-pip python-scipy python-pytables python-sqlalchemy python-matplotlib python-openpyxl python-xlrd python-xlwt python-xlsxwriter python-beautifulsoup4
